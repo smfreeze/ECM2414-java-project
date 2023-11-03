e993c3ca146ea50158183c9b7a94815923409af0
SMFreeze <117759431+smfreeze@users.noreply.github.com>
2023-11-03 15:08:18 +0000

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,18 @@
-public class Player {
+import java.util.ArrayList;
 
+public class Player {
+    private int playerNumber;
+    private ArrayList<Card> cardsList = new ArrayList<Card>();
+
+    public Player(int playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+
+    public Card removeCard(int pos) {
+        return cardsList.remove(pos);
+    }
+
+    public void addCard(Card card) {
+        cardsList.add(card);
+    }
 }
