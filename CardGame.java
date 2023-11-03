cc670220bafed63bf364c81b1b691de198570577
SMFreeze <117759431+smfreeze@users.noreply.github.com>
2023-11-03 15:08:03 +0000

    Update CardGame.java

--- CardGame.java
+++ CardGame.java
@@ -1,3 +1,14 @@
-public class CardGame {
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
+public class CardGame {
+    public static void main(String[] args) {
+        System.out.println("Please enter the number of players:");
+        Scanner terminalReader = new Scanner(System.in);
+        String players = terminalReader.next();
+        System.out.println("Please enter location of pack to load:");
+        String pack = terminalReader.next();
+        terminalReader.close();
+    }
 }
