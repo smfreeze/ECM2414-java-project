50339e47a6c80733ff5886efdc67ec2b977c8419
SMFreeze <117759431+smfreeze@users.noreply.github.com>
2023-11-05 15:07:50 +0000

    Update CardGame.java

--- CardGame.java
+++ CardGame.java
@@ -4,11 +4,21 @@ import java.io.FileNotFoundException;
 
 public class CardGame {
     public static void main(String[] args) {
-        System.out.println("Please enter the number of players:");
-        Scanner terminalReader = new Scanner(System.in);
-        String players = terminalReader.next();
-        System.out.println("Please enter location of pack to load:");
-        String pack = terminalReader.next();
-        terminalReader.close();
+        // System.out.println("Please enter the number of players:");
+        // Scanner terminalReader = new Scanner(System.in);
+        // String players = terminalReader.next();
+        // System.out.println("Please enter location of pack to load:");
+        // String pack = terminalReader.next();
+        // terminalReader.close();
+
+        final Player test = new Player(1);
+        final Player test2 = new Player(2);
+        test.start();
+        test2.start();
+        test.print();
+        test.interrupt();
+        System.out.println("");
+        test.print();
+
     }
 }
