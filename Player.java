6af710d3bc090bbade3b58a0ceae975869763cce
SMFreeze <117759431+smfreeze@users.noreply.github.com>
2023-11-05 15:07:32 +0000

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,18 +1,34 @@
 import java.util.ArrayList;
 
-public class Player {
-    private int playerNumber;
+public class Player extends Thread {
+    private final int playerNumber;
+    private Boolean done = false;
     private ArrayList<Card> cardsList = new ArrayList<Card>();
+    private int count = 0;
+
+    public void run() {
+        while (!Thread.interrupted()) {
+            count++; // arbitrary calculation
+        }
+    }
 
     public Player(int playerNumber) {
         this.playerNumber = playerNumber;
     }
 
-    public Card removeCard(int pos) {
+    public synchronized Card removeCard(int pos) {
         return cardsList.remove(pos);
     }
 
-    public void addCard(Card card) {
+    public synchronized void addCard(Card card) {
         cardsList.add(card);
+    }
+
+    public synchronized void stopThread() {
+        done = true;
+    }
+
+    public synchronized void print() {
+        System.out.println(this.getName() + "Printed");
     }
 }
